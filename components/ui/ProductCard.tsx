4b6fbdf1073ef190afd18176f5e3a676804ad62a
lindsey5 <lindseysamson5@gmail.com>
2026-03-11 23:54:31 +0800

    feat: optimize ProductCard for responsive layout and remove redundant width calculations in ProductList

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -1,12 +1,26 @@
-import { View, Image, StyleSheet, TouchableOpacity, ViewStyle } from "react-native"
+import { View, Image, StyleSheet, TouchableOpacity, Dimensions } from "react-native"
 import CustomizedText from "./Text";
 import useResponsiveFontSize from "@/hooks/useResponsiveFont";
 
-export default function ProductCard ({ item, style } : { item: Product, style?: ViewStyle  }) {
+const { width: screenWidth } = Dimensions.get("window");
+
+const getItemWidth = (numColumns : number, containerPadding = 20, spacing = 10) => {
+  return (screenWidth - containerPadding * 2 - spacing * (numColumns - 1)) / numColumns;
+};
+
+export default function ProductCard ({ item } : { item: Product  }) {
     const font20 = useResponsiveFontSize(20);
+    let numColumns = 2;
+
+    if (screenWidth > 900) {
+        numColumns = 4;
+    } else if (screenWidth > 600) {
+        numColumns = 3;
+    }
+    const itemWidth = getItemWidth(numColumns);
 
     return (
-        <View style={[styles.card, style]}>
+        <View style={[styles.card, { width: itemWidth }]}>
             <Image
                 source={
                     typeof item.image === "string"
--- components/ui/ProductList.tsx
+++ components/ui/ProductList.tsx
@@ -1,26 +1,11 @@
 import { products } from "@/constants/data";
-import { View, StyleSheet, Dimensions } from "react-native";
+import { View, StyleSheet } from "react-native";
 import ProductCard from "./ProductCard";
 
-const { width: screenWidth } = Dimensions.get("window");
-
-const getItemWidth = (numColumns : number, containerPadding = 20, spacing = 10) => {
-  return (screenWidth - containerPadding * 2 - spacing * (numColumns - 1)) / numColumns;
-};
-
 export default function ProductList() {
-    let numColumns = 2;
-
-    if (screenWidth > 900) {
-        numColumns = 4;
-    } else if (screenWidth > 600) {
-        numColumns = 3;
-    }
-    const itemWidth = getItemWidth(numColumns);
-    
     return (
         <View style={styles.container}>
-            {products.map(item => <ProductCard item={item} key={item.id} style={{ width: itemWidth }}/>)}
+            {products.map(item => <ProductCard item={item} key={item.id} />)}
         </View>
     );
 }
