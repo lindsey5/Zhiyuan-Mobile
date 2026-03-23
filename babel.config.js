bfa4c112c0ac57adbfcc1556a690b8a0a6d9b99c
lindsey5 <lindsey5@gmail.com>
2026-03-23 23:12:26 +0800

    commit

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,8 @@ module.exports = function(api) {
     return {
         presets: ['babel-preset-expo'],
         plugins: [
+        'expo-router/babel',
+
         ['inline-dotenv', {
             path: '.env',
         }],
