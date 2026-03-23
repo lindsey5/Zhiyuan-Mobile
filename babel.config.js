dffbb402fd0cf5b77acd65a5275a77e567666af2
lindsey5 <lindsey5@gmail.com>
2026-03-23 23:32:27 +0800

    fix error

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
-module.exports = function(api) {
+module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: [
-        ['inline-dotenv', {
-            path: '.env',
-        }],
-        ],
     };
 };
