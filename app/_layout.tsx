b3cf9648fd950e2f63ec494e4afaeddc72f2b1ad
lindsey5 <lindsey5@gmail.com>
2026-03-29 08:10:57 +0800

    fix: update projectId in app.json and enhance navigation bar setup in _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
+import * as NavigationBar from 'expo-navigation-bar';
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +14,18 @@ export default function RootLayout() {
     if (!fontsLoaded) {
         return null;
     }
+
+    async function setupNavBar() {
+      // Hide nav bar
+      await NavigationBar.setVisibilityAsync("hidden");
+
+      // Enable swipe to temporarily show it
+      await NavigationBar.setBehaviorAsync("overlay-swipe");
+    }
+
+    useEffect(() => {
+        setupNavBar();
+    }, []);
 
     return (
         <QueryClientProvider client={queryClient}>
