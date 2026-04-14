dd0eeb7f3d5577f9463715cedc176f1f77340fe9
lindseysamson5 <lindseysamson5@gmail.com>
2026-04-14 11:29:39 +0800

    commit

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,6 +1,5 @@
 import ProductScreen from "@/components/Product/ProductScreen";
 import GradientBackground from "@/components/ui/GradientBackgroud";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Page() {
 
